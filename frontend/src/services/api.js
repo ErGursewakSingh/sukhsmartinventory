766df555d3375507a53e9ee2bbeaea01ce8ed697
ErGursewakSingh <gursewak.bfcet@gmail.com>
2026-04-05 23:47:18 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "baseURL: import.meta.env.VITE_API_URL"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 // ✅ Attach token to every request
